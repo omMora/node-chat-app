05d22889c733abfa269e196b213696226517cc59
ionic2 <o.elkhand@gmail.com>
2017-01-05 03:57:31 +0000

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -14,15 +14,32 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
-
+    
+    //socket.emit from Admin text Welcome to the chat app
     socket.emit('newMessage', {
-        from: 'test',
-        text: 'hello from server',
-        createdAt: 1234
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+    //socket.broadcast.emit from Admin text New user joined
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
     });
 
-    socket.on('createMessage', (newMessage) => {
-        console.log('createMessage', newMessage);
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
