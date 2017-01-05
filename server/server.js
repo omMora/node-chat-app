4d1444b4e5c33671af9fb5b73f42ae7a94620df3
ionic2 <o.elkhand@gmail.com>
2017-01-05 03:24:18 +0000

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'test',
+        text: 'hello from server',
+        createdAt: 1234
+    });
+
+    socket.on('createMessage', (newMessage) => {
+        console.log('createMessage', newMessage);
+    });
+
     socket.on('disconnect', () => {
         console.log('Client disconnected');
     });
