41a37ce3f6c1e781487d4a3be8642baaa45cf21f
ionic2 <o.elkhand@gmail.com>
2017-01-17 00:50:00 +0000

    Improving user experience

--- public/js/index.js
+++ public/js/index.js
@@ -16,11 +16,45 @@ socket.on('newMessage', function (message) {
     jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My Current location</a>');
+
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
 
+    var messageTextbox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
-    }, function() {});
+        text: messageTextbox.val()
+    }, function() {
+        messageTextbox.val('');
+    });
+});
+
+var locationButton = jQuery('#send-location');
+
+locationButton.on('click', function() {
+    if(!navigator.geolocation) {
+        return alert('Geolocation not suppoerted by your browser');
+    }
+
+    locationButton.attr('disabled', 'disabled');
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        // console.log(position);
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function() {
+        alert('Unable to fetch location');
+    });
 });
